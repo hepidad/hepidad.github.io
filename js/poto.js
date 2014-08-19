6c5cbedb7a8ccb0d7cd5d0b9a2cb8d230aafe183
hepidad <hepidad@gmail.com>
2014-08-20 07:04:31 +0900

    api sig add

--- js/poto.js
+++ js/poto.js
@@ -24,7 +24,8 @@ $(function () {
             method: 'flickr.photosets.getPhotos',
             //api_key: '7617adae70159d09ba78cfec73c13be3'
             api_key: '5f6398f0051388362b59218103af392c',
-            photoset_id: '72157646413131665'
+            photoset_id: '72157646413131665',
+            api_sig: '02dd23063e5a80f4edd8964931b37ebd',
         },
         dataType: 'jsonp',
         jsonp: 'jsoncallback'
